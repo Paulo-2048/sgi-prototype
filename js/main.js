fe957df26ad0e6e965cf8abc6791a5f74b483d21
Paulo Vitor <paulo19032004@gmail.com>
2022-02-02 18:18:01 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+try {
+  window.Cookies.get("token")
+} catch (e) {
+  window.location.replace = "../login.html"
+}
+const h1Text = document.querySelector("#test-header")
+window.axios
+  .get("https://sgi-prototype-api.vercel.app/")
+  .then(function (response) {
+    // handle success
+    console.log(response.data)
+    h1Text.innerHTML = response.data.data
+  })
+  .catch(function (error) {
+    // handle error
+    console.error(error)
+  })
