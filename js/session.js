be4874b7effb8ebf793620d87923ed8c12d7fd16
Paulo Vitor <paulo19032004@gmail.com>
2022-02-06 11:58:59 -0300

    Correctin session.js

--- js/session.js
+++ js/session.js
@@ -1,8 +1,9 @@
-const email = document.querySelector('#inputEmail')
-const password = document.querySelector('#inputPassword')
 const login = document.querySelector('#login-btn')
 
 login.addEventListener('click', () => {
+  const email = document.querySelector('#inputEmail').value
+  const password = document.querySelector('#inputPassword').value
+  
   window.axios
   .post("https://sgi-prototype-api.vercel.app/user/login", {
       email: email,
