9992aa4c64d4ec12e42166b34888ea19fbed6e27
Paulo Vitor <paulo19032004@gmail.com>
2022-02-06 12:01:43 -0300

    debug session

--- js/session.js
+++ js/session.js
@@ -3,6 +3,8 @@ const login = document.querySelector('#login-btn')
 login.addEventListener('click', () => {
   const email = document.querySelector('#inputEmail').value
   const password = document.querySelector('#inputPassword').value
+  localStorage.setItem('email', email)
+  localStorage.setItem('senha', password)
   
   window.axios
   .post("https://sgi-prototype-api.vercel.app/user/login", {
