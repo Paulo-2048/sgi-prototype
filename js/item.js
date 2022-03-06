9107dfd3264b69b3649cf6319ff730eac2c4ddfd
Paulo Vitor <paulo19032004@gmail.com>
2022-03-06 00:48:09 -0300

    List Itens test

--- js/item.js
+++ js/item.js
@@ -29,15 +29,15 @@ if (location.href == "https://sgi-prototype.vercel.app/item-post.html") {
 
 // List all itens
 if (location.href == "https://sgi-prototype.vercel.app/item-list.html") {
-  let table = document.querySelector("#dataTable_item")
-  let nRow = table.rows.length
+  let tb = document.querySelector("#dataTable_item")
+  let nRow = tb.rows.length
   try {
     $(document).ready(function () {
       $("#dataTable_item").DataTable()
     })
 
     const listItem = async () => {
-      let newRow = await table.insertRow(nRow - 1)
+      let newRow = await tb.insertRow(nRow - 1)
       newRow.insertCell(0).innerHTML = "OK0"
       newRow.insertCell(1).innerHTML = "OK1"
       newRow.insertCell(2).innerHTML = "OK2"
