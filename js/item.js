5f566c7a7c7b1675fee1bf21998ce13e6fdc1d75
Paulo Vitor <paulo19032004@gmail.com>
2022-03-06 00:05:33 -0300

    List itens test

--- js/item.js
+++ js/item.js
@@ -1,21 +1,39 @@
-document.querySelector("#item-post-btn").addEventListener("click", () => {
-  let itemForm = document.querySelector("#item-form")
+// Post New itens
+if (location.href == "https://sgi-prototype.vercel.app/item-post.html") {
+  try {
+    document.querySelector("#item-post-btn").addEventListener("click", () => {
+      let itemForm = document.querySelector("#item-form")
 
-  window.axios
-    .post("https://sgi-prototype-api.vercel.app/item", {
-      name: itemForm.item_name.value,
-      category: itemForm.item_category.value,
-      description: itemForm.item_description.value,
-      price: itemForm.item_price.value,
-      client: itemForm.item_client.value,
+      window.axios
+        .post("https://sgi-prototype-api.vercel.app/item", {
+          name: itemForm.item_name.value,
+          category: itemForm.item_category.value,
+          description: itemForm.item_description.value,
+          price: itemForm.item_price.value,
+          client: itemForm.item_client.value,
+        })
+        .then((response) => {
+          alert("Postagem de item realizado com sucesso")
+          location.reload()
+        })
+        .catch((error) => {
+          // handle error
+          console.error(error)
+          alert("Um erro foi encontrado")
+        })
     })
-    .then((response) => {
-      alert("Postagem de item realizado com sucesso")
-      location.reload()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// List all itens
+if (location.href == "https://sgi-prototype.vercel.app/item-list.html") {
+  try {
+    $(document).ready(function () {
+      $("#dataTable_item").DataTable()
     })
-    .catch((error) => {
-      // handle error
-      console.error(error)
-      alert("Um erro foi encontrado")
-    })
-})
+  } catch (error) {
+    console.error(error)
+  }
+}
